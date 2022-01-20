b7101f40da858f36835a0fadc665b41012313af9
Kevin Colyar <kevinc@dcpud.org>
2022-01-20 15:31:24 -0800

    Adds side planes in scene

--- src/main.rs
+++ src/main.rs
@@ -52,27 +52,46 @@ fn main() {
             normal: Vector::default(), // TODO: Not needed for Sphere
             material: Material { diffuse: Color { r: 0.0, g: 1.0, b: 0.0 }, reflection: 0.2, power: 60.0 }
         },
+        // Bottom
         Object {
             object_type: ObjectType::Plane,
             position: Vector { x: 0.0, y: -200.0, z: 0.0 },
             radius: 0.0, // TODO: Not needed for Plane
             normal: Vector { x: 0.0, y: 1.0, z: 0.0 },
-            material: Material { diffuse: Color { r: 0.5, g: 0.0, b: 0.5 }, reflection: 0.5, power: 60.0 }
+            material: Material { diffuse: Color { r: 0.5, g: 0.8, b: 0.8 }, reflection: 0.5, power: 60.0 }
         },
+        // Top
         Object {
             object_type: ObjectType::Plane,
             position: Vector { x: 0.0, y: 200.0, z: 0.0 },
             radius: 0.0, // TODO: Not needed for Plane
             normal: Vector { x: 0.0, y: -1.0, z: 0.0 },
-            material: Material { diffuse: Color { r: 0.5, g: 0.9, b: 0.5 }, reflection: 0.3, power: 60.0 }
+            material: Material { diffuse: Color { r: 0.5, g: 0.5, b: 0.5 }, reflection: 0.8, power: 60.0 }
         },
 
+        // Left
         Object {
-            object_type: ObjectType::Sphere,
-            position: Vector { x: -120.0, y: 0.0, z: 0.0 },
+            object_type: ObjectType::Plane,
+            position: Vector { x: -200.0, y: 0.0, z: 0.0 },
             radius: 100.0, // TODO: Not needed for Plane
             normal: Vector { x: 1.0, y: 0.0, z: 0.0 },
             material: Material { diffuse: Color { r: 1.0, g: 0.0, b: 0.0 }, reflection: 0.3, power: 60.0 }
+        },
+        // Right
+        Object {
+            object_type: ObjectType::Plane,
+            position: Vector { x: 200.0, y: 0.0, z: 0.0 },
+            radius: 100.0, // TODO: Not needed for Plane
+            normal: Vector { x: -1.0, y: 0.0, z: 0.0 },
+            material: Material { diffuse: Color { r: 0.0, g: 0.0, b: 1.0 }, reflection: 0.3, power: 60.0 }
+        },
+        // Back
+        Object {
+            object_type: ObjectType::Plane,
+            position: Vector { x: 0.0, y: 0.0, z: -500.0 },
+            radius: 100.0, // TODO: Not needed for Plane
+            normal: Vector { x: 0.0, y: 0.0, z: 1.0 },
+            material: Material { diffuse: Color { r: 1.0, g: 1.0, b: 1.0 }, reflection: 0.3, power: 60.0 }
         }
 
     ];
@@ -81,16 +100,16 @@ fn main() {
     let lights = vec![
         Light {
             position: Vector { x: 500.0, y: 500.0, z: 500.0 },
-            color: Color { r: 1.0, g: 0.0, b: 1.0 }
+            color: Color { r: 1.0, g: 1.0, b: 1.0 }
         },
         Light {
             position: Vector { x: -500.0, y: 500.0, z: 500.0 },
-            color: Color { r: 0.0, g: 1.0, b: 1.0 }
+            color: Color { r: 1.0, g: 1.0, b: 1.0 }
         }
     ];
 
     // Create eye
-    let eye = Vector { x: 0.0, y: 0.0, z: 500.0 };
+    let eye = Vector { x: 0.0, y: 100.0, z: 500.0 };
 
     // Create plane
     let plane = 100.0;
